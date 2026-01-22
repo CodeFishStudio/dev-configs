3f6d7b6ce9c2b04e4e409a9fc2c15e0ba9191206
Sam Jusaitis <sam.jusaitis@codefishstudio.com>
2026-01-22 14:56:31 +1030

    v0.0.23

--- src/configs/eslint/reactNext.template.ts
+++ src/configs/eslint/reactNext.template.ts
@@ -6,7 +6,7 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 
-const eslintConfig = [
+const eslintConfig = defineConfig([
     ...nextVitals,
     ...nextTs,
     ...eslintConfigs.reactNext,
@@ -17,7 +17,7 @@ const eslintConfig = [
         'build/**',
         'next-env.d.ts',
     ]),
-];
+]);
 
 export default eslintConfig;
 `;
